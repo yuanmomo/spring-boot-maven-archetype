693447e88db0a8ddfdd756aa6ff563164d16d437
Hongbin.Yuan <yuanhongbin@foxdsp.com>
2017-04-03 23:08:30 +0800

    init demo

---
+++ src/main/java/net/yuanmomo/springboot/Application.java
@@ -0,0 +1,38 @@
+package net.yuanmomo.springboot; /**
+ * Created by Hongbin.Yuan on 2017-04-03 20:04.
+ */
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan("net.yuanmomo.springboot")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+}
+
