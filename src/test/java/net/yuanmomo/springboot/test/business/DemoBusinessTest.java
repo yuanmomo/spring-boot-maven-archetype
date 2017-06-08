db350d7e390946489562d0878dcb09050ee20a08
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2017-06-08 17:07:13 +0800

    add pagination plugin test code

--- src/test/java/net/yuanmomo/springboot/test/business/DemoBusinessTest.java
+++ src/test/java/net/yuanmomo/springboot/test/business/DemoBusinessTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 /**
  * Created by Hongbin.Yuan on 2017-06-08 16:47.
  */
@@ -31,6 +33,31 @@ public class DemoBusinessTest extends BaseUnitTest {
 
         demo = this.demoMapper.selectOneByExample(demoParam);
         Assert.assertTrue(demo.getVersion() == 2222222);
+    }
+
+    @Test
+    @Sql("classpath:sql/demo-business-test.sql")
+    public void testPagination() throws Exception {
+        DemoParam demoParam = new DemoParam();
+        demoParam.setOrderByClause(" id desc ");
+
+        demoParam.setStart(1);
+        List<Demo> demoList = this.demoMapper.selectByExample(demoParam);
+        Assert.assertTrue(demoList.size() == 1);
+        Assert.assertTrue(demoList.get(0).getVersion() == 2222222);
+
+        demoParam.setCount(1);
+        demoParam.setStart(-1);
+        demoList = this.demoMapper.selectByExample(demoParam);
+        Assert.assertTrue(demoList.size() == 1);
+        Assert.assertTrue(demoList.get(0).getVersion() == 2222222);
+
+        demoParam.setStart(3);
+        demoParam.setCount(2);
+        demoList = this.demoMapper.selectByExample(demoParam);
+        Assert.assertTrue(demoList.size() == 2);
+        Assert.assertTrue(demoList.get(0).getVersion() == 5555555);
+        Assert.assertTrue(demoList.get(1).getVersion() == 9999999);
     }
 
 }
